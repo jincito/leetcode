ab25e0bcba29ee9693fc3ea8a3df28af40b2946b
Jin <110742524+jincito@users.noreply.github.com>
2024-06-07 14:00:10 -0400

    Complete LeetCode practice

---
+++ 217-Contains-Duplicate/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> sets = new HashSet<Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if(sets.contains(nums[i])){
+                return true;
+            }
+            else {
+                sets.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
